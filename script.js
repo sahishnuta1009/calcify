c4df68ff7ec71db92347aec845b73d541c266943
sahishnuta <sahishnuta25@gmail.com>
2026-02-21 18:44:55 +0530

    Cleaned formatting

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-  let calculation=JSON.parse(localStorage.getItem('calculation')) || '';
-   displayCalc();
+ let calculation = JSON.parse(localStorage.getItem('calculation')) || '';
 
+displayCalc();
 
-     function updateCalculation(value){
-       calculation+=value;
-       localStorage.setItem('calculation',JSON.stringify(calculation));
-       displayCalc();
-     }
+function updateCalculation(value) {
+  calculation += value;
+  localStorage.setItem('calculation', JSON.stringify(calculation));
+  displayCalc();
+}
 
-     function calcFinal(){
-      calculation=eval(calculation);
-       localStorage.setItem('calculation',JSON.stringify(calculation));
-      displayCalc();
-     }
-     
-     function displayCalc(){
-      const calc=document.querySelector('.display-calculation');
-      calc.innerText=calculation;
-     }
-      function clearCalc(){
-      calculation='';
-      localStorage.removeItem('calculation');
-      displayCalc();
-    }
+function calcFinal() {
+  calculation = eval(calculation);
+  localStorage.setItem('calculation', JSON.stringify(calculation));
+  displayCalc();
+}
+
+function displayCalc() {
+  const calc = document.querySelector('.display-calculation');
+  calc.innerText = calculation;
+}
+
+function clearCalc() {
+  calculation = '';
+  localStorage.removeItem('calculation');
+  displayCalc();
+}
